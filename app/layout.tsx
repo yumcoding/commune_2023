bb72ed4e68d4722e733bbe015b189aec4057c831
yumcoding <ymseo1014@gmail.com>
2023-06-13 22:48:38 +0900

    parallell routes, intercepting routes 이용해서 모달 보여주기 작업 중

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,13 +7,14 @@ export const metadata = {
 	description: "느슨한 취향 공동체",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ modal, children }: { modal: React.ReactNode; children: React.ReactNode }) {
 	return (
 		<html lang="ko">
 			<body className={noto_sans.className}>
 				<Header />
 				{children}
 				<MobileFooter />
+				{modal}
 			</body>
 		</html>
 	);
