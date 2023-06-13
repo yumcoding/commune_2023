3cca4d4a83d0d9fac820eda6162af882f930aa4a
yumcoding <ymseo1014@gmail.com>
2023-06-13 15:38:30 +0900

    루트 레이아웃에 헤더 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { noto_sans } from "./fonts";
 import MobileFooter from "@/components/common/Layout/MobileFooter";
+import Header from "@/components/common/Layout/Header";
 export const metadata = {
 	title: "Commune",
 	description: "느슨한 취향 공동체",
@@ -10,6 +11,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 	return (
 		<html lang="ko">
 			<body className={noto_sans.className}>
+				<Header />
 				{children}
 				<MobileFooter />
 			</body>
