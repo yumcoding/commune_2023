599c9db32dc6742f088520a0747a5c3986bbdf0a
yumcoding <ymseo1014@gmail.com>
2023-06-15 11:58:41 +0900

    레이아웃 메인 콘텐츠 영역 마진 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import "./globals.css";
 import { noto_sans } from "./fonts";
 import MobileFooter from "@/components/common/Layout/MobileFooter";
 import Header from "@/components/common/Layout/Header";
+import styles from "./styles.module.scss";
+
 export const metadata = {
 	title: "Commune",
 	description: "느슨한 취향 공동체",
@@ -12,7 +14,7 @@ export default function RootLayout({ modal, children }: { modal: React.ReactNode
 		<html lang="ko">
 			<body className={noto_sans.className}>
 				<Header />
-				{children}
+				<div className={styles.main}>{children}</div>
 				<MobileFooter />
 				{modal}
 			</body>
