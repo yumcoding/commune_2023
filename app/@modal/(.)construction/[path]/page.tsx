e18bc3cc60cc42b1c46ad7a71ce4c45c46468a51
yumcoding <ymseo1014@gmail.com>
2023-06-15 17:20:33 +0900

    준비중 페이지 콘텐츠 리팩토링 및 스타일 수정

--- app/@modal/(.)construction/[path]/page.tsx
+++ app/@modal/(.)construction/[path]/page.tsx
@@ -1,36 +1,10 @@
-"use client";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
 import ModalOverlay from "@/components/common/Modal/ModalOverlay";
-import { BulbIcon, CloseMarkIcon } from "@/assets/icons";
-import styles from "./styles.module.scss";
-import useBlockBodyScroll from "@/hooks/useBlockBodyScroll";
+import ConstructionContent from "@/components/common/ConstructionContent";
 
 export default function Page() {
-	const router = useRouter();
-
-	const onClickClose = () => router.back();
-
-	useBlockBodyScroll();
-
 	return (
-		<ModalOverlay onClick={onClickClose}>
-			<div className={styles.contentWrapper}>
-				<div>
-					<BulbIcon />
-					<div className={styles.text}>
-						<p>준비중입니다.</p>
-						<p>조금만 기다려주세요!</p>
-					</div>
-				</div>
-
-				<button type="button" className={styles.closeBtn} onClick={onClickClose}>
-					닫기
-				</button>
-				<button type="button" className={styles.closeIconBtn} onClick={onClickClose}>
-					<CloseMarkIcon />
-				</button>
-			</div>
+		<ModalOverlay>
+			<ConstructionContent isModal={true} />
 		</ModalOverlay>
 	);
 }
---
+++ components/common/ConstructionContent/index.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { BulbIcon, CloseMarkIcon } from "@/assets/icons";
+import { cls } from "@/lib/front/cls";
+import styles from "./styles.module.scss";
+
+export default function ConstructionContent({ isModal }: { isModal: boolean }) {
+	const { contentWrapper, modalWrapper, pageWrapper, text, backBtn, backIconBtn } = styles;
+	const router = useRouter();
+	const onClickClose = () => router.back();
+	return (
+		<>
+			<div className={cls(contentWrapper, isModal ? modalWrapper : pageWrapper)}>
+				<BulbIcon />
+				<div className={text}>
+					<p>준비중입니다.</p>
+					<p>조금만 기다려주세요!</p>
+				</div>
+				<button type="button" className={backBtn} onClick={onClickClose}>
+					뒤로가기
+				</button>
+				{isModal && (
+					<button type="button" className={backIconBtn} onClick={onClickClose}>
+						<CloseMarkIcon />
+					</button>
+				)}
+			</div>
+		</>
+	);
+}
