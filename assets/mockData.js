5ad3d5c39b3616dad8e1ad98e3f037edfebd3ecf
yumcoding <ymseo1014@gmail.com>
2023-06-16 18:18:37 +0900

    최근 검색어, 인기 검색어 mock data 추가

---
+++ assets/mockData.js
@@ -0,0 +1,45 @@
+export const recentSearch = [
+	{
+		id: 0,
+		query: "민음사",
+	},
+	{
+		id: 1,
+		query: "문학동네",
+	},
+	{
+		id: 2,
+		query: "믿음사",
+	},
+	{
+		id: 3,
+		query: "문학동태",
+	},
+	{
+		id: 4,
+		query: "문학과지성사",
+	},
+];
+
+export const popularSearch = [
+	{
+		id: 0,
+		query: "Clean Code",
+	},
+	{
+		id: 1,
+		query: "오늘도 개발자가 안 된다고 말했다",
+	},
+	{
+		id: 2,
+		query: "Refactoring: Improving the Design of Existing Code  (2nd Edition) (Addison-Wesley Signature Series (Fowler))",
+	},
+	{
+		id: 3,
+		query: "지구 끝의 온실",
+	},
+	{
+		id: 4,
+		query: "개발자의 글쓰기",
+	},
+];
