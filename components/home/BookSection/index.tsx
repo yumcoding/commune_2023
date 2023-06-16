6ec840e34d16c57fc0eaa5432c855eaf00dd33c8
yumcoding <ymseo1014@gmail.com>
2023-06-16 11:51:36 +0900

    swiper breakpoint 옵션 사용하여 slides 개수 화면 사이즈에 맞게 동적으로 변경

--- components/home/BookSection/index.tsx
+++ components/home/BookSection/index.tsx
@@ -3,10 +3,8 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper";
-
 import Book from "@/components/common/Book";
 import styles from "./styles.module.scss";
-import { useState, useEffect } from "react";
 
 interface PropTypes {
 	title: string;
@@ -15,32 +13,23 @@ interface PropTypes {
 export default function BookSection({ title }: PropTypes) {
 	const { heading, bookList, bookItem } = styles;
 
-	const [numOfSlides, setNumOfSlides] = useState(3);
-
-	useEffect(() => {
-		function handleResize() {
-			let vw = window.innerWidth;
-			if (vw < 720) {
-				setNumOfSlides(3);
-			} else if (vw >= 720 && vw < 1366) {
-				setNumOfSlides(4);
-			} else {
-				setNumOfSlides(5);
-			}
-		}
-
-		window.addEventListener("resize", handleResize);
-
-		handleResize();
-
-		return () => window.removeEventListener("resize", handleResize);
-	}, []);
-
 	const swiperParams = {
-		slidesPerView: numOfSlides,
+		slidesPerView: 3,
+		breakpoints: {
+			720: {
+				slidesPerView: 4,
+			},
+			1366: {
+				slidesPerView: 5,
+			},
+		},
 		spaceBetween: 10,
 		navigation: true,
 		modules: [Navigation],
+		// navigation: {
+		// 	nextEl: ".swiper-button-next",
+		// 	prevEl: ".swiper-button-prev",
+		// },
 	};
 
 	return (
