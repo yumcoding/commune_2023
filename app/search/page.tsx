b9001ed39eeeb2ddb87b0a4bbd74dd611d72ecf4
yumcoding <ymseo1014@gmail.com>
2023-06-19 11:18:52 +0900

    검색 페이지 태블릿/데탑용 마크업, 스타일링 추가 작업 중

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,18 +1,43 @@
 import SearchForm from "@/components/search/SearchForm";
 import styles from "./styles.module.scss";
 import SearchRecommendation from "@/components/search/SearchRecommendation";
+import { NoResultIcon } from "@/assets/icons";
+
+// 검색 페이지는 완전 CSR로 구현?
 
 export default function Search() {
-	const { mobileSearch } = styles;
+	const { mobileSearch, defaultSearch, queryWrapper, searchResultWrapper, noResult, resultList } = styles;
 
 	return (
 		<>
-			<div className={mobileSearch}>
+			{/* 모바일 */}
+			<section className={mobileSearch}>
 				<SearchForm />
+				{/* 모바일 - 검색 전 */}
 				<SearchRecommendation isRecent={true} />
 				<SearchRecommendation isRecent={false} />
-			</div>
-			{/* tablet 이후 search 화면 */}
+				{/* 모바일 - 검색 후 */}
+				{/* suspense 사용하여 로딩 이미지 보여주기 */}
+			</section>
+			{/* 태블릿 이후 */}
+			<section className={defaultSearch}>
+				<div className={queryWrapper}>
+					<div>&quot;{}&quot;의 검색 결과</div>
+				</div>
+
+				<div className={searchResultWrapper}>
+					{/* 검색 결과 없음 */}
+					<div className={noResult}>
+						<NoResultIcon />
+						<p>
+							검색 결과가 없어요. <br /> 다른 검색어를 입력해보세요.
+						</p>
+					</div>
+
+					{/* 검색 결과 */}
+					{/* <ul className={resultList}></ul> */}
+				</div>
+			</section>
 		</>
 	);
 }
