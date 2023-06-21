cb37f46aa08475e62971d7c551d1652c887363eb
yumcoding <ymseo1014@gmail.com>
2023-06-21 19:13:54 +0900

    리뷰 작성 모달 작업 중

---
+++ app/book/[isbn]/@writeReviewModal/(.)write-review/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+import PageModalOverlay from "@/components/common/Modal/PageModalOverlay";
+import styles from "./styles.module.scss";
+import { CloseMarkIcon } from "@/assets/icons";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { cls } from "@/lib/front/cls";
+export default function WriteReview() {
+	const { wrapper, header, closeBtn, saveBtn, saveActive, formWrapper, counter } = styles;
+
+	const router = useRouter();
+	const onClickClose = () => router.back();
+
+	const [review, setReview] = useState("");
+	const onChangeReview = (e: React.ChangeEvent<HTMLTextAreaElement>) => setReview(e.target.value);
+	return (
+		<PageModalOverlay>
+			<section className={wrapper}>
+				<header className={header}>
+					<h1>책 이름 자리</h1>
+					<button type="button" className={closeBtn} onClick={onClickClose}>
+						<CloseMarkIcon />
+					</button>
+				</header>
+				<div className={formWrapper}>
+					<form>
+						<textarea placeholder="자유롭게" value={review} onChange={onChangeReview}></textarea>
+						<button type="submit" className={cls(saveBtn, review?.length > 0 ? saveActive : "")} disabled={review?.length > 0 ? false : true}>
+							저장
+						</button>
+					</form>
+					<p className={counter}>{review?.length} / 1000</p>
+				</div>
+			</section>
+		</PageModalOverlay>
+	);
+}
---
+++ app/book/[isbn]/write-review/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.scss";
+
+export default function Page() {
+	return (
+		<>
+			<div className={styles.test}>새로고침화면</div>
+		</>
+	);
+}
