3575f1684b4075e4cc290917519f3674d39d8fff
yumcoding <ymseo1014@gmail.com>
2023-06-24 17:04:04 +0900

    책 리스트 carousel 컴포넌트로 분리하여 적용

--- components/home/BookSection/index.tsx
+++ components/home/BookSection/index.tsx
@@ -1,60 +1,16 @@
-"use client";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/navigation";
-import { Navigation } from "swiper";
-import Book from "@/components/common/Book";
 import styles from "./styles.module.scss";
-import { cls } from "@/lib/front/cls";
-import useSwiperRef from "@/hooks/useSwiperRef";
-import { ChevronLeftIcon, ChevronRightIcon } from "@/assets/icons";
+import BookSwiper from "@/components/common/BookSwiper";
 
 interface PropTypes {
 	title: string;
 }
 
 export default function BookSection({ title }: PropTypes) {
-	const { section, heading, swiperWrapper, btn, prevBtn, nextBtn } = styles;
-
-	const [prevEl, prevElRef] = useSwiperRef<HTMLButtonElement>();
-	const [nextEl, nextElRef] = useSwiperRef<HTMLButtonElement>();
-
-	const swiperParams = {
-		slidesPerView: 3,
-		breakpoints: {
-			720: {
-				slidesPerView: 4,
-			},
-			1366: {
-				slidesPerView: 5,
-			},
-		},
-		spaceBetween: 10,
-		modules: [Navigation],
-		navigation: {
-			prevEl,
-			nextEl,
-		},
-	};
-
+	const { section, heading } = styles;
 	return (
 		<section className={section}>
 			<h2 className={heading}>{title}</h2>
-			<div className={swiperWrapper}>
-				<Swiper {...swiperParams}>
-					{[0, 1, 2, 3, 4, 5, 6, 7, 8].map((item, i) => (
-						<SwiperSlide key={i}>
-							<Book />
-						</SwiperSlide>
-					))}
-				</Swiper>
-				<button type="button" ref={prevElRef} className={cls(btn, prevBtn)}>
-					<ChevronLeftIcon />
-				</button>
-				<button type="button" ref={nextElRef} className={cls(btn, nextBtn)}>
-					<ChevronRightIcon />
-				</button>
-			</div>
+			<BookSwiper hasShowMore={false} />
 		</section>
 	);
 }
