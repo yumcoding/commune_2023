de19ac7cf863bff6afff424731f22bf555ac756e
yumcoding <ymseo1014@gmail.com>
2023-06-24 16:59:55 +0900

    Book component 클릭 시 이동하는 href 임시 수정 및 TODO 리스트 작성

--- components/common/Book/index.tsx
+++ components/common/Book/index.tsx
@@ -1,6 +1,14 @@
 import Link from "next/link";
 import styles from "./styles.module.scss";
 
+// TODO 1
+// href 두 군데 넣어주어야 한다.
+
+// TODO 2
+// href에 넣어주는 값이 책 상세 페이지인지 리뷰인지 구분
+// props로 detail인지 review인지 구분해서
+// ? '/book/isbn'으로 갈지 '/review'로 갈지 구분하는 방법
+
 export default function Book() {
 	const { book, bookImg, bookDesc } = styles;
 	return (
@@ -14,7 +22,7 @@ export default function Book() {
 				</Link>
 
 				<div className={bookDesc}>
-					<Link href="/">
+					<Link href="/book/temp-isbn">
 						<h3>책 이름</h3>
 						<div>
 							<strong>저자, 옮긴이</strong>
