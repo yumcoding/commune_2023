980b7f1f2226eb5e2b5c850142f6f5606a63e673
yumcoding <ymseo1014@gmail.com>
2023-07-16 18:06:15 +0900

    검색어 입력 시 검색어 context 값 설정

--- components/search/SearchForm/index.tsx
+++ components/search/SearchForm/index.tsx
@@ -1,21 +1,30 @@
 "use client";
 
-import { FormEvent, useState } from "react";
+import { FormEvent, useState, useRef, useContext } from "react";
 import styles from "./styles.module.scss";
 import { CloseMarkIcon, SearchIcon } from "@/assets/icons";
+import { SearchQueryContext } from "@/providers/searchQueryProvider";
+
 export default function SearchForm() {
 	const { form, searchIconWrapper, searchClearBtn } = styles;
-	const [query, setQuery] = useState("");
+
+	const inputRef = useRef<HTMLInputElement>(null);
+
+	const { setQuery } = useContext(SearchQueryContext);
+
 	const onSubmit = (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
+			setQuery(inputRef.current?.value);
+		
 	};
+
 	return (
 		<>
 			<form onSubmit={onSubmit} className={form}>
 				<div className={searchIconWrapper}>
 					<SearchIcon />
 				</div>
-				<input type="text" />
+				<input type="text" ref={inputRef} />
 				<button type="button" className={searchClearBtn}>
 					<CloseMarkIcon />
 				</button>
