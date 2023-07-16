54e29718053977304a47824e338f7567ba60cd2c
yumcoding <ymseo1014@gmail.com>
2023-07-16 14:09:17 +0900

    AuthSessionProvider 파일 이름 및 함수 이름 변경

--- providers/authSessionProvider.tsx
+++ providers/authSessionProvider.tsx
@@ -2,6 +2,6 @@
 
 import { SessionProvider } from "next-auth/react";
 
-export default function AuthSession({ children }: { children: React.ReactNode }) {
+export default function AuthSessionProvider({ children }: { children: React.ReactNode }) {
 	return <SessionProvider>{children}</SessionProvider>;
 }
