a422b3741e26c6ebf883ab52ae0abd4e79aa47ee
yumcoding <ymseo1014@gmail.com>
2023-07-20 19:03:52 +0900

    jsx 속 class -> className으로 수정

--- assets/icons.js
+++ assets/icons.js
@@ -15,7 +15,7 @@ export const LoginIcon = () => (
 );
 
 export const LogOutIcon = () => (
-	<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="w-6 h-6">
+	<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
 		<path
 			strokeLinecap="round"
 			strokeLinejoin="round"
