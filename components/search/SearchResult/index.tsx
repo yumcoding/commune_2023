2ae95a532cc402f4b98f8c4f02bc371617265457
yumcoding <ymseo1014@gmail.com>
2023-08-01 16:48:33 +0900

    SearchResultSection, SearchSection api 찌르는 구조 변경 및 pagination 적용

---
+++ components/search/SearchResult/index.tsx
@@ -0,0 +1,61 @@
+"use client";
+import useSWR from "swr";
+import { searchFetcher } from "@/lib/front/fetchers";
+import Book from "@/components/common/Book";
+import { NoResultIcon } from "@/assets/icons";
+import styles from "./styles.module.scss";
+import Image from "next/image";
+import Loader from "@/components/common/Loader";
+
+export interface ItemTypes {
+	title: string;
+	link: string;
+	image: string;
+	author: string;
+	discount: string;
+	publisher: string;
+	pubdate: string;
+	isbn: string;
+	description: string;
+}
+
+export interface SearchResultTypes {
+	lastBuildDate: string;
+	total: number;
+	start: number;
+	display: number;
+	items: ItemTypes[];
+}
+
+export const PAGE_SIZE = 10;
+
+export default function SearchResult({ query, pageIndex }: { query: string; pageIndex: number }) {
+	const { wrapper, list, noResult } = styles;
+
+	const { data, isLoading, error } = useSWR<SearchResultTypes>(
+		query?.length > 0 ? `/openapi/v1/search/book.json?query=${query}&display=${PAGE_SIZE}&start=${PAGE_SIZE * pageIndex + 1}` : null,
+		searchFetcher
+	);
+
+	if (isLoading) return <Loader />;
+
+	return (
+		<ul>
+			{data?.items.map((item) => (
+				<li key={item.isbn}>
+					<div>
+						<Image src={item.image} width={50} height={50} alt={`${item.title} 책 커버`} style={{ background: "grey" }} />
+					</div>
+					<div>
+						<h1>{item.title}</h1>
+						<p>
+							<strong>
+								{item.author.replaceAll("^", ", ")} | {item.publisher}
+							</strong>
+						</p>
+					</div>
+				</li>
+			))}
+		</ul>
+	);
+}
--- components/search/SearchResultSection/index.tsx
+++ components/search/SearchResultSection/index.tsx
@@ -1,58 +1,27 @@
 "use client";
-import useSWR from "swr";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { SearchQueryContext } from "@/providers/searchQueryProvider";
-import { searchFetcher } from "@/lib/front/fetchers";
-import { NoResultIcon } from "@/assets/icons";
+import SearchResult from "../SearchResult";
 import styles from "./styles.module.scss";
-import SearchResultList from "../SearchResultList";
-
-export interface ItemTypes {
-	title: string;
-	link: string;
-	image: string;
-	author: string;
-	discount: string;
-	publisher: string;
-	pubdate: string;
-	isbn: string;
-	description: string;
-}
-
-export interface SearchResultTypes {
-	lastBuildDate: string;
-	total: number;
-	start: number;
-	display: number;
-	items: ItemTypes[];
-}
+import Pagination from "@/components/common/Pagination";
 
 export default function SearchResultSection() {
-	const { wrapper, searchHeader, noResult } = styles;
+	const { wrapper, searchHeader } = styles;
 
 	const { query } = useContext(SearchQueryContext);
-	const { data } = useSWR<SearchResultTypes>(query && query?.length > 0 ? `/openapi/v1/search/book.json?query=${query}` : null, searchFetcher);
+
+	const [pageIndex, setPageIndex] = useState(0);
 
 	return (
 		<section className={wrapper}>
 			<header className={searchHeader}>
 				<div>&quot;{query}&quot;의 검색 결과</div>
 			</header>
-
-			{!data ? null : (
-				<>
-					{data.total === 0 ? (
-						<div className={noResult}>
-							<NoResultIcon />
-							<p>
-								검색 결과가 없어요. <br /> 다른 검색어를 입력해보세요.
-							</p>
-						</div>
-					) : (
-						<SearchResultList items={data.items} />
-					)}
-				</>
-			)}
+			<SearchResult query={query ?? ""} pageIndex={pageIndex} />
+			<div style={{ display: "none" }}>
+				<SearchResult query={query ?? ""} pageIndex={pageIndex + 1} />
+			</div>
+			<Pagination query={query ?? ""} pageIndex={pageIndex} setPageIndex={setPageIndex} />
 		</section>
 	);
 }
