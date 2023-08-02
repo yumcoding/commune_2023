8bd36c811a5b5acbb239b8ead2c983065994a9d9
yumcoding <ymseo1014@gmail.com>
2023-08-02 13:49:22 +0900

    pagination 기능 및 스타일링 추가

--- components/common/Pagination/index.tsx
+++ components/common/Pagination/index.tsx
@@ -3,9 +3,22 @@ import ReactPaginate from "react-paginate";
 import useSWR from "swr";
 import { searchFetcher } from "@/lib/front/fetchers";
 import { PAGE_SIZE } from "@/components/search/SearchResult";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+import styles from "./styles.module.scss";
+interface PageClickEventTypes {
+	index: number | null;
+	selected: number;
+	nextSelectedPage: number | undefined;
+	event: object;
+	isPrevious: boolean;
+	isNext: boolean;
+	isBreak: boolean;
+	isActive: boolean;
+}
 
 export default function Pagination({ query, pageIndex }: { query: string; pageIndex: number }) {
+	const { hidden, wrapper, list, item, active,ellipsis } = styles;
+
 	const { data, isLoading } = useSWR(query?.length > 0 ? `/openapi/v1/search/book.json?query=${query}&display=${PAGE_SIZE}&start=${PAGE_SIZE * (pageIndex - 1) + 1}` : null, searchFetcher);
 
 	const getPageCount = () => {
@@ -21,15 +34,31 @@ export default function Pagination({ query, pageIndex }: { query: string; pageIn
 	};
 
 	const router = useRouter();
-	function handlePageClick(selectedItem: { selected: number }): void {
-		router.push(`/search?query=${query}&pageIndex=${selectedItem.selected}`);
-	}
+
+	const onClick = (e: PageClickEventTypes) => {
+		const nextPageIndex = e.nextSelectedPage ? e.nextSelectedPage + 1 : 1;
+		router.push(`/search?query=${query}&pageIndex=${nextPageIndex}`);
+	};
 
 	if (isLoading) return null;
 
 	return (
-		<>
-			<ReactPaginate pageCount={getPageCount()} onPageChange={handlePageClick} pageRangeDisplayed={5} breakLabel="..." nextLabel=">" previousLabel="<" renderOnZeroPageCount={null} />
-		</>
+		<div className={wrapper}>
+			<ReactPaginate
+				pageCount={getPageCount()}
+				onClick={onClick}
+				pageRangeDisplayed={5}
+				marginPagesDisplayed={2}
+				previousClassName={hidden}
+				nextClassName={hidden}
+				containerClassName={list}
+				pageClassName={item}
+				activeClassName={active}
+				breakClassName={ellipsis}
+				initialPage={pageIndex - 1}
+				breakLabel="..."
+				renderOnZeroPageCount={null}
+			/>
+		</div>
 	);
 }
