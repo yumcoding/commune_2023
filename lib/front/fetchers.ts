b2613f02429c1847765fa79dd95ffae67e17dab4
yumcoding <ymseo1014@gmail.com>
2023-08-03 13:50:19 +0900

    api 응답이 xml로 오는 경우의 fetcher 함수 추가

--- lib/front/fetchers.ts
+++ lib/front/fetchers.ts
@@ -1,3 +1,5 @@
+import convertXMLtoJSON from "./convertXMLtoJSON";
+
 const requestHeaders = new Headers();
 requestHeaders.set("Content-Type", "application/json");
 requestHeaders.set("X-Naver-Client-Id", process.env.NEXT_PUBLIC_NAVER_CLIENT_ID ? process.env.NEXT_PUBLIC_NAVER_CLIENT_ID : "");
@@ -7,3 +9,20 @@ export const searchFetcher = (url: string) =>
 	fetch(url, {
 		headers: requestHeaders,
 	}).then((res) => res.json());
+
+export const searchFetcherXML = (url: string) => {
+	fetch(url, {
+		headers: requestHeaders,
+	})
+		.then((res) => res.text())
+		.then((xmlString) => {
+			const xmlNode = new DOMParser().parseFromString(xmlString, "text/xml");
+			const json: any = convertXMLtoJSON(xmlNode);
+			const { rss } = json;
+			console.log(rss.channel.item);
+			return rss.channel.item;
+		})
+		.catch((e) => {
+			console.error(e);
+		});
+};
