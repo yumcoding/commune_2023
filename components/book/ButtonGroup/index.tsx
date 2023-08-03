c4f56f8f6a875077e9977c96214832cd4d555554
yumcoding <ymseo1014@gmail.com>
2023-08-03 15:19:20 +0900

    책 상세 페이지 헤더에서 책 구매 창으로 넘어가도록 링크 추가

--- components/book/ButtonGroup/index.tsx
+++ components/book/ButtonGroup/index.tsx
@@ -1,11 +1,14 @@
 "use client";
 
-import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useParams } from "next/navigation";
+import useSWR from "swr";
 import useScrollDownCheck from "@/hooks/useScrollDownCheck";
 import { cls } from "@/lib/front/cls";
 import { ArrowLeftIcon, CartIcon, LinkIcon } from "@/assets/icons";
 import styles from "./styles.module.scss";
+import { BookDescTypes } from "../BookInfo";
+import { searchFetcherXML } from "@/lib/front/fetchers";
 
 export default function ButtonGroup() {
 	const { wrapper, leftGroup, hasScrolled } = styles;
@@ -14,6 +17,14 @@ export default function ButtonGroup() {
 
 	const router = useRouter();
 	const goBack = () => router.back();
+
+	const params = useParams();
+	const { data } = useSWR<BookDescTypes>(`/openapi/v1/search/book_adv.xml?d_isbn=${params.isbn}`, searchFetcherXML, {
+		revalidateIfStale: false,
+		revalidateOnFocus: false,
+		revalidateOnReconnect: false,
+	});
+
 	return (
 		<>
 			<div className={cls(wrapper, hasScrolledDown ? hasScrolled : "")}>
@@ -21,12 +32,16 @@ export default function ButtonGroup() {
 					<ArrowLeftIcon />
 				</button>
 				<div className={leftGroup}>
-					<button type="button">
-						<LinkIcon />
-					</button>
-					<Link href="/">
-						<CartIcon />
-					</Link>
+					{data && (
+						<>
+							<button type="button">
+								<LinkIcon />
+							</button>
+							<a href={data.link} target="_blank">
+								<CartIcon />
+							</a>
+						</>
+					)}
 				</div>
 			</div>
 		</>
