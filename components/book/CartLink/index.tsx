8d21003dd9cfa35797c0444b35b9dd5c0df6b4d5
yumcoding <ymseo1014@gmail.com>
2023-08-03 16:50:42 +0900

    태블릿 이상 화면에서 노출되는 카트 링크 기능 추가

--- components/book/CartLink/index.tsx
+++ components/book/CartLink/index.tsx
@@ -1,11 +1,29 @@
+"use client";
+import { useParams } from "next/navigation";
+import useSWR from "swr";
+import { searchFetcherXML } from "@/lib/front/fetchers";
 import { CartIcon } from "@/assets/icons";
 import styles from "./styles.module.scss";
+import { BookDescTypes } from "../BookInfo";
+
 export default function CartLink() {
+	// 책 구매 페이지 띄우기
+	const params = useParams();
+	const { data } = useSWR<BookDescTypes>(`/openapi/v1/search/book_adv.xml?d_isbn=${params.isbn}`, searchFetcherXML, {
+		revalidateIfStale: false,
+		revalidateOnFocus: false,
+		revalidateOnReconnect: false,
+	});
+
 	return (
-		<div className={styles.cart}>
-			<a href="/" target="_blank">
-				<CartIcon />
-			</a>
-		</div>
+		<>
+			{data && (
+				<div className={styles.cart}>
+					<a href={data.link} target="_blank">
+						<CartIcon />
+					</a>
+				</div>
+			)}
+		</>
 	);
 }
