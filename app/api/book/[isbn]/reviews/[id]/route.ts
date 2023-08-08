2f02b8160aab605f99bb62de70003b773d7a4f18
yumcoding <ymseo1014@gmail.com>
2023-08-08 20:27:37 +0900

    특정 isbn && 특정 리뷰 mutation위한 route.ts파일 임시 생성

--- app/api/book/[isbn]/reviews/[id]/route.ts
+++ app/api/book/[isbn]/reviews/[id]/route.ts
@@ -1,1 +1,23 @@
-// 특정 책의 특정 리뷰 하나 GET, PATCH, DELETE
+// import prisma from "@/lib/server/prisma";
+// import { NextResponse } from "next/server";
+
+// // 특정 책, 특정 사용자가 쓴 특정 리뷰를 GET, PATCH, DELETE
+// export async function GET({ params }: { params: { isbn: string } }) {
+// 	const response = await prisma.$queryRaw`SET SESSION sql_mode = 'STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';`.then(async () => {
+// 		const review = await prisma.review.findMany({
+// 			where: {
+// 				bookIsbn: params.isbn,
+// 			},
+// 			include: {
+// 				user: {
+// 					select: {
+// 						name: true,
+// 						image: true,
+// 					},
+// 				},
+// 			},
+// 		});
+// 		return review;
+// 	});
+// 	return NextResponse.json({ ok: true, review: response });
+// }
