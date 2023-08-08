1138b34fdc1bda90411b4f96d22ae50b0f41e556
yumcoding <ymseo1014@gmail.com>
2023-08-08 20:27:06 +0900

    특정 isbn에 작성된 리뷰 전체 get, 해당 isbn에 리뷰 작성 post 위한 route handler 작성

--- app/api/book/[isbn]/reviews/route.ts
+++ app/api/book/[isbn]/reviews/route.ts
@@ -1,33 +1,35 @@
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import prisma from "@/lib/server/prisma";
-import { getServerSession } from "next-auth";
-import { getSession } from "next-auth/react";
+import { getServerSession } from "next-auth/next";
 import { NextResponse } from "next/server";
 
 // reviews 전체 GET
-export async function GET() {
+export async function GET(req: Request, { params }: { params: { isbn: string } }) {
 	const response = await prisma.$queryRaw`SET SESSION sql_mode = 'STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';`.then(async () => {
 		const reviews = await prisma.review.findMany({
+			where: {
+				bookIsbn: params.isbn,
+			},
 			include: {
-				_count: {
+				user: {
 					select: {
-						likes: true,
+						name: true,
+						image: true,
 					},
 				},
 			},
 		});
 		return reviews;
 	});
-	return NextResponse.json({ ok: true, reviews: response });
+	return NextResponse.json({ ok: true, data: response });
 }
 
 // review 작성
-export async function POST(request: Request) {
-	// const session = await getServerSession(authOptions);
-	// console.log("session!!!!!!!!!!!!!!!!!", session);
-	const body = await request.json();
-	// console.log(body);
-	const { title, content, bookIsbn, bookAuthor, bookTitle, bookImage, user } = body;
+export async function POST(req: Request) {
+	const session = await getServerSession(authOptions);
+	const body = await req.json();
+
+	const { title, content, bookIsbn, bookAuthor, bookTitle, bookImage } = body;
 	const review = await prisma.review.create({
 		data: {
 			title,
@@ -38,7 +40,7 @@ export async function POST(request: Request) {
 			bookImage,
 			user: {
 				connect: {
-					email: user.email,
+					id: session.user.id,
 				},
 			},
 		},
