b46a5212ba193393839215be50f83b02b64d7852
yumcoding <ymseo1014@gmail.com>
2023-08-11 18:29:09 +0900

    가로 로더 아이콘 추가

--- assets/icons.js
+++ assets/icons.js
@@ -203,6 +203,20 @@ export const LoaderIcon = () => (
 	</svg>
 );
 
+export const HorizontalLoaderIcon = () => (
+	<svg version="1.1" id="L4" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100" enableBackground="new 0 0 0 0" xmlSpace="preserve">
+		<circle fill="#ccc" stroke="none" cx="6" cy="50" r="6">
+			<animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin="0.1" />
+		</circle>
+		<circle fill="#ccc" stroke="none" cx="26" cy="50" r="6">
+			<animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin="0.2" />
+		</circle>
+		<circle fill="#ccc" stroke="none" cx="46" cy="50" r="6">
+			<animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin="0.3" />
+		</circle>
+	</svg>
+);
+
 export const CartIcon = () => (
 	<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
 		<path
