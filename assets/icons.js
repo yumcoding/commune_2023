f72591176c48cd3be2cfd2d448910e4b8aaf9326
yumcoding <ymseo1014@gmail.com>
2023-08-17 20:49:30 +0900

    UserDefaultIcon 굵기 수정

--- assets/icons.js
+++ assets/icons.js
@@ -25,7 +25,7 @@ export const LogOutIcon = () => (
 );
 
 export const UserDefaultIcon = () => (
-	<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+	<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1" stroke="currentColor" className="w-6 h-6">
 		<path
 			strokeLinecap="round"
 			strokeLinejoin="round"
