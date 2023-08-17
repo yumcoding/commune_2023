7e716c7d65bc731efc25f8bde1e8ea748c378b99
yumcoding <ymseo1014@gmail.com>
2023-08-17 20:55:00 +0900

    내서재 페이지에 새로 생성한 섹션 컴포넌트들 적용

--- app/library/page.tsx
+++ app/library/page.tsx
@@ -1,11 +1,11 @@
 import Nav from "@/components/library/Nav";
-import styles from "./styles.module.scss";
-import BookSwiper from "@/components/common/BookSwiper";
-import { NoListItemIcon } from "@/assets/icons";
+
 import UserInfoSection from "@/components/library/UserInfoSection";
+import styles from "./styles.module.scss";
+import UserReviewSection from "@/components/library/UserReviewSection";
 
 export default function Page() {
-	const { main, reviewSection, noItem } = styles;
+	const { main } = styles;
 
 	return (
 		<main className={main}>
@@ -13,18 +13,7 @@ export default function Page() {
 
 			<UserInfoSection />
 
-			<section className={reviewSection}>
-				<h1>
-					작성한 리뷰 <small>(527)</small>
-				</h1>
-				{/* 작성한 책 리뷰 없을 때  */}
-				<div className={noItem}>
-					<NoListItemIcon />
-					<p>아직 작성한 리뷰가 없어요.</p>
-				</div>
-				{/* 작성한 책 리뷰 있을 때  */}
-				{/* <BookSwiper hasShowMore={true} /> */}
-			</section>
+			<UserReviewSection />
 		</main>
 	);
 }
