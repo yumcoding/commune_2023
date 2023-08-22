aeb136e1bb359e591ec19598c9330add8fbc0a1e
yumcoding <ymseo1014@gmail.com>
2023-08-22 14:49:23 +0900

    DB 바로 찔러서 리뷰 많은 책 리스트 보여주는 컴포넌트 생성

---
+++ components/home/DBBookList/index.tsx
@@ -0,0 +1,26 @@
+import BookSwiper from "@/components/common/BookSwiper";
+import { cache } from "react";
+import prisma from "@/lib/server/prisma";
+
+export const revalidate = 3600;
+
+export const getReviews = cache(async () => {
+	const reviews = await prisma.review.findMany({
+		take: 10,
+		orderBy: {
+			likes: {
+				_count: "desc",
+			},
+		},
+	});
+	return reviews;
+});
+
+export default async function DBBookList() {
+	const reviews = await getReviews();
+	return (
+		<>
+			<BookSwiper hasShowMore={false} list={reviews} />
+		</>
+	);
+}
