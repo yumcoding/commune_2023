0bbeca420f180a0648432d06a803a02471796e71
yumcoding <ymseo1014@gmail.com>
2023-08-24 17:04:30 +0900

    ListLoading 컴포넌트 윈도우 사이즈 별로 보여주는 슬라이드 숫자 변경되도록 적용

--- components/home/ListLoading/index.tsx
+++ components/home/ListLoading/index.tsx
@@ -1,11 +1,24 @@
+"use client";
+import { useState, useEffect } from "react";
 import styles from "./styles.module.scss";
 
 export default function ListLoading() {
 	const { list, bookImg, bookDesc } = styles;
 
+	const [width, setWidth] = useState(window.innerWidth);
+
+	useEffect(() => {
+		const handleResize = () => setWidth(window.innerWidth);
+		window.addEventListener("resize", handleResize);
+
+		return () => window.removeEventListener("resize", handleResize);
+	}, []);
+
+	const sliderNum = width < 720 ? 3 : width < 1366 ? 4 : 5;
+
 	return (
 		<ul className={list}>
-			{Array.from({ length: 5 }).map((_, i) => (
+			{Array.from({ length: sliderNum }).map((_, i) => (
 				<li key={crypto.randomUUID()}>
 					<div className={bookImg} />
 					<div className={bookDesc} />
