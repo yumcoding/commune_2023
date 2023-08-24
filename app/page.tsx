d074ffa19cb279a0dd32f654d4237b3b850ae3f5
yumcoding <ymseo1014@gmail.com>
2023-08-24 19:09:53 +0900

    홈 화면 - 최근 작성된 리뷰 리스트 적용 및 섹션 헤딩 스타일링 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import DBBookList from "@/components/home/DBBookList";
 import styles from "./styles.module.scss";
-import { Suspense } from "react";
-import ListLoading from "@/components/home/ListLoading";
 import SearchBookList from "@/components/home/SearchBookList";
 export default function Page() {
 	const { main, section, sectionHeading } = styles;
@@ -10,23 +8,26 @@ export default function Page() {
 			<main className={main}>
 				<section className={section}>
 					<h2 className={sectionHeading}>
-						리뷰가 가장 많은 책 TOP 10
-						<span>👑</span>
+						최근 리뷰가 작성된 책<span>✏️</span>
 					</h2>
-					<Suspense fallback={<ListLoading />}>
-						<DBBookList />
-					</Suspense>
+					<DBBookList />
 				</section>
 				<section className={section}>
-					<h2 className={sectionHeading}>가을맞이 추천 시리즈 1. &#39;띵&#39; </h2>
+					<h2 className={sectionHeading}>
+						가을<span>🍂</span> 추천 시리즈 1. &#39;띵&#39;{" "}
+					</h2>
 					<SearchBookList query="띵 시리즈" />
 				</section>
 				<section className={section}>
-					<h2 className={sectionHeading}>가을맞이 추천 시리즈 2. &#39;아무튼&#39;</h2>
+					<h2 className={sectionHeading}>
+						가을<span>🍂</span> 추천 시리즈 2. &#39;아무튼&#39;
+					</h2>
 					<SearchBookList query="아무튼" />
 				</section>
 				<section className={section}>
-					<h2 className={sectionHeading}>가을맞이 추천 시리즈 3. &#39;일상이 고고학&#39;</h2>
+					<h2 className={sectionHeading}>
+						가을<span>🍂</span> 추천 시리즈 3. &#39;일상이 고고학&#39;
+					</h2>
 					<SearchBookList query="일상이 고고학" />
 				</section>
 			</main>
