b25287221bba9616ed973b930a83528c36b4a89d
yumcoding <ymseo1014@gmail.com>
2023-09-02 17:37:34 +0900

    로그인하지 않은 사용자는 좋아요 버튼 누르지 못하도록 수정

--- components/book/ReviewItem/IsLikedBtn/index.tsx
+++ components/book/ReviewItem/IsLikedBtn/index.tsx
@@ -5,11 +5,13 @@ import { useParams } from "next/navigation";
 import { ReviewItemTypes } from "@/types/db";
 import { fetcher } from "@/lib/front/fetchers";
 import styles from "./styles.module.scss";
+import { useSession } from "next-auth/react";
 
 export default function IsLikedBtn(props: { reviewId: number }) {
 	const { container, isLikedWrapper, checkbox, heart, mainCirc, grp1, grp2, grp3, grp4, grp5, grp6, grp7, oval1, oval2, count } = styles;
 
 	const params = useParams();
+	const session = useSession();
 
 	const [toggleLikes] = useMutation(`/api/book/${params.isbn}/reviews/${props.reviewId}/likes`, "POST");
 
@@ -39,7 +41,7 @@ export default function IsLikedBtn(props: { reviewId: number }) {
 	return (
 		<div className={container}>
 			<form className={isLikedWrapper}>
-				<input type="checkbox" id="checkbox" className={checkbox} checked={reviewItemData?.isLiked} onChange={onChangeCheckbox} />
+				<input type="checkbox" id="checkbox" className={checkbox} disabled={session?.status === "unauthenticated"} checked={reviewItemData?.isLiked} onChange={onChangeCheckbox} />
 				<label htmlFor="checkbox">
 					<svg id="heart-svg" viewBox="467 392 58 57" xmlns="http://www.w3.org/2000/svg">
 						<g id="Group" fill="none" fill-rule="evenodd" transform="translate(467 392)">
